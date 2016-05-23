a7b31dd8209ee76a3d27a784a1127970c307bd39
Valentin <navshop@ukr.net>
2016-05-23 15:26:44 +0300

    change showModal on show

--- js/dialog.js
+++ js/dialog.js
@@ -1,14 +1,14 @@
 (() => 
 {
 	"use strict"
-	console.log("привет")
+	
 	const $ = selector => document.querySelector(selector)
 
-	console.log($("body"))
+	
 	$("body").addEventListener("click", () =>
 	{
-		console.log(event.target)
-		$("dialog").showModal();
+	
+		$("dialog").show();
 	})
 
 
