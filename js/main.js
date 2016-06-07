5e81ce0102de5fae7d0ba3162d6879dc78a0791f
Valentin <navshop@ukr.net>
2016-06-07 18:45:34 +0300

    added file main.js

---
+++ js/main.js
@@ -0,0 +1,31 @@
+(() =>
+{
+	"use strict"
+
+	const $ = selector => document.querySelector(selector)
+
+// Change name category
+	$('.groups_title').textContent = "Каталог товаров"
+
+// Change div class=groups on nav class=groups
+	{
+		let fragment = document.createDocumentFragment()
+		let $nav = document.createElement('nav')
+		$nav.setAttribute('class', 'groups')
+		fragment.appendChild($nav).innerHTML = $('.groups').innerHTML
+		$('.left').replaceChild(fragment, $('.groups'))
+
+		console.log(fragment)
+	}
+	
+// Change div class=groups_title on h2 class=groups_title
+	{
+		let fragment = document.createDocumentFragment()
+		let $h2 = document.createElement('h2')
+		$h2.setAttribute('class', 'groups')
+		fragment.appendChild($h2).textContent 
+									= $('.groups_title').textContent
+		$('.groups').replaceChild(fragment, $('.groups_title'))
+	}
+
+})()
